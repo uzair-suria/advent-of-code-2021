e66da7be9d2202270dbf25c8cfc9b4a715824613
uzair-suria <uzair.a.suria@outlook.com>
2021-12-01 11:00:08 +0500

    parsing to int when reading data

--- day-1/solution-1-1.js
+++ day-1/solution-1-1.js
@@ -5,12 +5,12 @@ const dataInput = fs.readFileSync("./input.txt", {
   flag: "r",
 });
 
-const input = dataInput.split("\n");
+const input = dataInput.split("\n").map((val) => parseInt(val));
 
 let count = 0;
 for (let i = 1; i < input.length; i++) {
-  const curr = parseInt(input[i]);
-  const prev = parseInt(input[i - 1]);
+  const curr = input[i];
+  const prev = input[i - 1];
   if (curr > prev) {
     count++;
     console.log(typeof curr);
